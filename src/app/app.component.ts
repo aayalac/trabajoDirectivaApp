f2fc70d26324f734d04809d07284e439ba5b5f52
Miguel Angel Ayala <ingeniero.ayala@outlook.com>
2024-02-14 12:47:58 -0500

    feat: se crea formulario para registrar usuarios empleando las directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+interface Entrada {
+  titulo: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, FormsModule, NgStyle,NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
+
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje = "";
+  registrado = false;
+  nombre:string="";
+  apellido:string="";
+  cargo:string="";
+  entradas: Entrada[];
+
+  constructor() {
+    this.entradas = [
+      {titulo: "Python cada dia mas presente"},
+      {titulo: "Java presente hace mas de 20 años"},
+      {titulo: "Javascript cada vez mas funcional"},
+      {titulo: "Kotlin potencia tus apps"},
+      {titulo: "¿Donde quedó pascal?"}
+    ];
+  }
+
+  registrarUsuario() {
+    this.registrado = true
+    this.mensaje = "Usuario registrado con éxito!"
+  }
 }
